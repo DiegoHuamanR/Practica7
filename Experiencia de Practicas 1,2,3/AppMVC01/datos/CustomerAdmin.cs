8fb70c503eefd9ad25fd7da55ff3d9a98ec6093a
DiegoHuamanR <72468973@ucsm.edu.pe>
2021-10-13 22:23:43 -0500

    Experiencia de Practicas 1,2 y 3

---
+++ Experiencia de Practicas 1,2,3/AppMVC01/datos/CustomerAdmin.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AppMVC01.Models;
+namespace AppMVC01.datos
+{
+    public class CustomerAdmin
+    {
+        public IEnumerable<CLIENTES> Consultar()
+        {
+            using (VentasEntities contexto = new VentasEntities())
+            {
+                return contexto.CLIENTES.AsNoTracking().ToList();
+            }
+        }
+        public CLIENTES Consultar(int id)
+        {
+            using (VentasEntities contexto = new VentasEntities())
+            {
+                return contexto.CLIENTES.AsNoTracking().FirstOrDefault(c =>
+               c.codigo == id);
+            }
+        }
+        public void Guardar(CLIENTES modelo)
+        {
+            using (VentasEntities contexto = new VentasEntities())
+            {
+            contexto.CLIENTES.Add(modelo);
+                contexto.SaveChanges();
+            }
+        }
+        public void Modificar(CLIENTES modelo)
+        {
+            using (VentasEntities contexto = new VentasEntities())
+            {
+                contexto.Entry(modelo).State =
+               System.Data.Entity.EntityState.Modified;
+                contexto.SaveChanges();
+            }
+        }
+        public void Eliminar(CLIENTES modelo)
+        {
+            using (VentasEntities contexto = new VentasEntities())
+            {
+                contexto.Entry(modelo).State =
+               System.Data.Entity.EntityState.Deleted;
+                contexto.SaveChanges();
+            }
+        }
+    }
+}
